cff874267afadfc37a162224768991fb20fd57f6
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2025-01-18 17:33:39 -0800

    tailwind works

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="columns-3">
+    <p>col1</p>
+    <p>col2</p>
+    <p>col3</p>
+</div>
