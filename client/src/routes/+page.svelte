facd01a6496a59a602f91de978257dda2ac90154
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2025-01-18 22:31:21 -0800

    connected websockets

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -4,36 +4,36 @@
     import Switch from './Switch.svelte'
     import { io } from 'socket.io-client'
 
-    const socket = io.connect('http://localhost:6969');
-
-    socket.on('eventFromServer', (message) => {
-        console.log(message);
-    })
-    // $: {
-    //     // send message to server
-    //     socket.emit('eventFromClient', $reactiveValue)
-    // }
-
     let switchValue;
-    let stack = ["sajhdashd","asdgsagdsd"];
     let code = {
-        text: ["bozo", "touch some grass"],
-        emoji: ["🍕🐘🎨", "🐝🌈📚✨🏖️🎲"]
+        program_text: ["..."],
+        program_emojis: ["..."],
+        stack: ["..."]
     };
+
+    const socket = io.connect('http://localhost:6969');
+    socket.on("connect", () => {
+        console.log(socket.id);
+    });
+
+    socket.on('state', (message) => {
+        console.log(message);
+        code = JSON.parse(message);
+    })
 </script>
 
 <div class="flex">
     <div class="w-full grid grid-cols-3">
         <div class="px-6 py-4 shadow-xl">
             <p class="text-lg font-bold">Runtime Stack</p>
-            <CodeEditor codeLines={stack}/>
+            <CodeEditor codeLines={code.stack}/>
         </div>
         <div>
             <Camera/>
         </div>
         <div class="px-6 py-4 shadow-xl">
             <Switch bind:value={switchValue} design="multi" options={['Text', 'Emoji']}/>
-            <CodeEditor codeLines={switchValue === "Text" ? code.text : code.emoji}/>
+            <CodeEditor codeLines={switchValue === "Text" ? code.program_text : code.program_emojis}/>
         </div>
     </div>
 </div>
