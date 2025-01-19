2f6172842761a318cb0dd955d09bccde13e625ab
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2025-01-19 10:55:25 -0800

    add logo

--- client/src/routes/Camera.svelte
+++ client/src/routes/Camera.svelte
@@ -46,29 +46,35 @@
     });
 </script>
 
-<div class="relative w-full h-screen bg-black">
-    <video
-            bind:this={videoElement}
-            autoplay
-            playsinline
-            class="w-full h-full object-cover"
-    ></video>
+<div class="flex flex-col items-center justify-center w-full h-screen bg-gray-900 gap-2">
+    <div class="inline-flex items-center gap-2">
+        <img src="/baboon.png" alt="The project logo" class="h-12 w-12" />
+        <p class="font-bold text-2xl">Baboon</p>
+    </div>
+    <div class="relative w-full h-screen">
+        <video
+                bind:this={videoElement}
+                autoplay
+                playsinline
+                class="w-full h-full object-cover"
+        ></video>
 
-    <div class="absolute bottom-5 left-1/2 transform -translate-x-1/2">
-        {#if isCameraActive}
-            <button
-                    on:click={stopCamera}
-                    class="px-6 py-2 bg-red-600 text-white font-semibold rounded-full shadow-lg"
-            >
-                Stop Camera
-            </button>
-        {:else}
-            <button
-                    on:click={startCamera}
-                    class="px-6 py-2 bg-blue-600 text-white font-semibold rounded-full shadow-lg"
-            >
-                Start Camera
-            </button>
-        {/if}
+        <div class="absolute bottom-5 left-1/2 transform -translate-x-1/2">
+            {#if isCameraActive}
+                <button
+                        on:click={stopCamera}
+                        class="px-6 py-2 bg-red-600 text-white font-semibold rounded-full shadow-lg"
+                >
+                    Stop Camera
+                </button>
+            {:else}
+                <button
+                        on:click={startCamera}
+                        class="px-6 py-2 bg-blue-600 text-white font-semibold rounded-full shadow-lg"
+                >
+                    Start Camera
+                </button>
+            {/if}
+        </div>
     </div>
 </div>
