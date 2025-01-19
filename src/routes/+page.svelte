f16191f3d17b57af1240ea02e856140274293eef
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2025-01-18 21:16:35 -0800

    Toggle changes the text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,28 @@
 <script>
-    import RuntimeStack from "./RuntimeStack.svelte";
     import CodeEditor from "./CodeEditor.svelte";
     import Camera from "./Camera.svelte";
+    import Switch from './Switch.svelte'
+
+    let switchValue;
     let stack = ["sajhdashd","asdgsagdsd"];
-    let codeLines = ["bozo", "touch some grass"];
+    let code = {
+        text: ["bozo", "touch some grass"],
+        emoji: ["🍕🐘🎨", "🐝🌈📚✨🏖️🎲"]
+    };
 </script>
 
 <div class="flex">
     <div class="w-full m-4 grid grid-cols-3 gap-4">
         <div>
-            <RuntimeStack stack={stack}/>
+            <p class="text-lg font-bold">Runtime Stack</p>
+            <CodeEditor codeLines={stack}/>
         </div>
         <div>
             <Camera/>
         </div>
         <div>
-            <CodeEditor codeLines={codeLines}/>
+            <Switch bind:value={switchValue} design="multi" options={['Text', 'Emoji']}/>
+            <CodeEditor codeLines={switchValue == "Emoji" ? code.emoji : code.text}/>
         </div>
     </div>
 </div>
--- src/routes/CodeEditor.svelte
+++ src/routes/CodeEditor.svelte
@@ -1,20 +1,13 @@
 <script>
-    import Switch from './Switch.svelte'
-
     export let codeLines;
-    let switchValue;
 </script>
 
-<div class="mb-4">
-    <Switch bind:value={switchValue} design="multi" options={['Emoji', 'Text']}/>
-</div>
-
-<div class="bg-gray-900 px-2 py-1 rounded-md">
+<div class="bg-gray-900 px-2 py-1 rounded-md mt-2">
     <table>
         <tbody>
         {#each codeLines as codeLine, i}
             <tr>
-                <td class="text-gray-400 w-8 font-mono">{i+1}</td>
+                <td class="text-gray-400 w-7 font-mono">{i+1}</td>
                 <td class="flex-1 font-mono">{codeLine}</td>
             </tr>
         {/each}
