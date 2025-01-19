0ff3f2100b477502b2ceefbe76cec3a3baaa86c7
jenn1705 <99060936+jentaruno@users.noreply.github.com>
2025-01-19 00:17:28 -0800

    try timeout toast

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -3,13 +3,16 @@
     import Camera from "./Camera.svelte";
     import Switch from './Switch.svelte'
     import { io } from 'socket.io-client'
+    import ErrorToast from "./ErrorToast.svelte";
 
     let switchValue;
     let code = {
         program_text: ["..."],
         program_emojis: ["..."],
-        stack: ["..."]
+        stack: ["..."],
+        error: ""
     };
+
 
     const socket = io.connect('http://localhost:6969');
     socket.on("connect", () => {
@@ -20,6 +23,15 @@
         console.log(message);
         code = JSON.parse(message);
     })
+
+    socket.on('error', (message) => {
+        console.log(message);
+        code.error = message;
+    })
+
+    function removeToast() {
+        code.error = ""
+    }
 </script>
 
 <div class="flex">
@@ -35,5 +47,10 @@
             <Switch bind:value={switchValue} design="multi" options={['Text', 'Emoji']}/>
             <CodeEditor codeLines={switchValue === "Text" ? code.program_text : code.program_emojis}/>
         </div>
+    </div>
+    <div class="fixed end-4 bottom-0">
+        {#if code.error && code.error.length > 0}
+            <ErrorToast error={code.error} on:change={removeToast} />
+        {/if}
     </div>
 </div>
---
+++ client/src/routes/ErrorToast.svelte
@@ -0,0 +1,37 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+    import { fade } from 'svelte/transition';
+
+    export let error;
+    let timer;
+    const dispatch = createEventDispatcher();
+
+    function deleteToast() {
+        dispatch('change');
+    }
+
+    $: if (error && error.length > 0) {
+        if (timer) {
+            clearTimeout(timer);
+        }
+        timer = setTimeout(function() { deleteToast(); }, 1000);
+    }
+</script>
+
+<div out:fade>
+    <div id="toast-danger" class="flex items-center w-full max-w-xs p-4 mb-4 text-white-500 bg-gray-700 rounded-lg shadow dark:text-white-400 dark:bg-gray-800" role="alert">
+        <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
+            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z"/>
+            </svg>
+            <span class="sr-only">Error icon</span>
+        </div>
+        <div class="ms-3 text-sm font-normal">Item has been deleted.</div>
+        <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-gray-700 text-white-400 hover:text-white-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-white-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-danger" aria-label="Close">
+            <span class="sr-only">Close</span>
+            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+            </svg>
+        </button>
+    </div>
+</div>
