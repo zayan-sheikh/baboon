c237632b512de7c3f0e55527c4bec69e1d7aa238
AdenC123 <39308648+AdenC123@users.noreply.github.com>
2025-01-19 08:49:19 -0800

    start and stop lang

--- server/main.py
+++ server/main.py
@@ -9,23 +9,39 @@ app = Flask(__name__)
 CORS(app)
 socketio = SocketIO(app, cors_allowed_origins='*')
 
-    
+running = True
+
+@socketio.on('stop')
+def handle_stop_event():
+    global running
+    running = False
+    print("Stopped")
+
+
+@socketio.on('start')
+def handle_start_event():
+    global running
+    running = True
+    print("Started")
+
+
 def send_state():
     last_pose = None
     while True:
-        # predict pose
-        pose = pose_estimation.get_pose()
-        if pose is not None and pose != 'neut' and pose != last_pose:
-            # new pose detected, try adding to model
-            try:
-                lang.doPose(pose)
-            except lang.LangException as err:
-                print(f"Error: {err}")
-                socketio.emit('error', str(err))
-            state = lang.getState()
-            socketio.emit('state', state)
-            print(f"Sent: {state}")
-        last_pose = pose
+        if running:
+            # predict pose
+            pose = pose_estimation.get_pose()
+            if pose is not None and pose != 'neut' and pose != last_pose:
+                # new pose detected, try adding to model
+                try:
+                    lang.doPose(pose)
+                except lang.LangException as err:
+                    print(f"Error: {err}")
+                    socketio.emit('error', str(err))
+                state = lang.getState()
+                socketio.emit('state', state)
+                print(f"Sent: {state}")
+            last_pose = pose
 
 
 if __name__ == "__main__":
